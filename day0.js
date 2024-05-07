08a799e5f0bb5128ab8328edb8c0282d2c5ad621
mavasquez401 <mavasquez401@gmail.com>
2024-05-06 21:12:01 -0400

    day002 Operators

---
+++ day0.js
@@ -0,0 +1,10 @@
+solve = (mealCost, tip, tax) => {
+    let tipPercent = (tip * mealCost)/100
+    let taxPercent = (tax * mealCost)/100
+    console.log(mealCost + tipPercent + taxPercent);
+    
+    return 
+}
+
+solve(100, 15, 8);
+
---
+++ day1_Data_Types.js
@@ -0,0 +1,10 @@
+solve = (mealCost, tip, tax) => {
+    let tipPercent = (tip * mealCost)/100
+    let taxPercent = (tax * mealCost)/100
+    let totalCost = mealCost + tipPercent + taxPercent
+    console.log(totalCost);
+    
+    return 
+}
+
+solve(100, 15.532, 8.25);
