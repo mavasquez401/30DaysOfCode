cc761d7af9fcd435705930cd7ed270511d08eb7a
mavasquez401 <mavasquez401@gmail.com>
2024-05-08 01:16:08 -0400

    Updated day1 due to errors

--- day1_Data_Types.js
+++ day1_Data_Types.js
@@ -1,18 +1,20 @@
-function main() {
-    var i = 4
-    var d = 4.0
-    var s = "HackerRank "
+function main(i,d,s) {
     // Declare second integer, double, and String variables.
-
+    
     // Read and save an integer, double, and String to your variables.
+    let i2 = 4
+    let d2 = 4.0
+    let s2 = "HackerRank "
     
     // Print the sum of both integer variables on a new line.
-    console.log(i + 12)
+    console.log(i + i2)
     // Print the sum of the double variables on a new line.
-    console.log((d + d).toFixed(5))
+    console.log((d + d2).toFixed(1))
     // Concatenate and print the String variables on a new line
-    // The 's' variable above should be printed first.
-    console.log(s + "is the best place to learn and practice coding!")
 
+    // The 's' variable above should be printed first.
+    console.log(s2 + s)
+    return
 }
-main()
+// Prints out data
+main(5,3.6,"is the best platform to learn code!");
