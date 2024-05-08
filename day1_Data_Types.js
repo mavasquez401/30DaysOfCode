caecd0d2cd8088a5741319b791db140dd12125e5
mavasquez401 <mavasquez401@gmail.com>
2024-05-07 23:32:00 -0400

    Updated day1

--- day1_Data_Types.js
+++ day1_Data_Types.js
@@ -1,10 +1,18 @@
-solve = (mealCost, tip, tax) => {
-    let tipPercent = (tip * mealCost)/100
-    let taxPercent = (tax * mealCost)/100
-    let totalCost = mealCost + tipPercent + taxPercent
-    console.log(totalCost);
-    
-    return 
-}
+function main() {
+    var i = 4
+    var d = 4.0
+    var s = "HackerRank "
+    // Declare second integer, double, and String variables.
 
-solve(100, 15.532, 8.25);
+    // Read and save an integer, double, and String to your variables.
+    
+    // Print the sum of both integer variables on a new line.
+    console.log(i + 12)
+    // Print the sum of the double variables on a new line.
+    console.log((d + d).toFixed(5))
+    // Concatenate and print the String variables on a new line
+    // The 's' variable above should be printed first.
+    console.log(s + "is the best place to learn and practice coding!")
+
+}
+main()
